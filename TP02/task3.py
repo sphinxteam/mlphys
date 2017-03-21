89f4d40205c3824d39b3d5c0ef68bb1ea072af9e
Eric W. Tramel <eric.tramel@gmail.com>
2017-03-21 02:42:07 +0100

    hrm

--- TP02/task3.py
+++ TP02/task3.py
@@ -1,12 +1,12 @@
-def power_features(x, maxPower):
+def power_features(x, max_power):
     """
         Given a vector of data points, x, build a matrix of power
         features from 0 (constant) up to power p for use with
         polynomial fitting.
     """
-    X = np.zeros((x.shape[0], maxPower+1))
+    X = np.zeros((x.shape[0], max_power+1))
     
-    for p in range(0,maxPower+1):
+    for p in range(0,max_power+1):
         X[:,p] = np.power(x,p)    
     
     return X
